3a49ba9e2029d1500a04e17f75e1c3cc690e73f8
prog_zidane <mohamed.zidane95@gmail.com>
2021-01-01 15:09:51 +0200

    init App LifeCycle

---
+++ Memorize/AppLifeCycle/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Memorize
+//
+//  Created by prog_zidane on 1/1/21.
+//
+
+import UIKit
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("hello from appDelegate")
+        return true
+    }
+}
---
+++ Memorize/AppLifeCycle/MemorizeApp.swift
@@ -0,0 +1,47 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by prog_zidane on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) var scenePhase
+    
+    init() {
+        setupApp()
+    }
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                print("Acitive")
+            case .background:
+                print("background")
+            case .inactive:
+                print("inactive")
+            default:
+                print("new state added by apple")
+            }
+        }
+    }
+}
+
+extension MemorizeApp {
+    
+    func setupApp() {
+        
+    }
+}
+
+
+
